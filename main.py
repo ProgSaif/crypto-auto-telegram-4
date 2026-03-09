1a5898e2a3f5e1d6a36c337abac6bfdb7b58255c
ProgSaif <isaifulone@gmail.com>
2026-03-09 10:15:34 +0600

    Update main.py

--- main.py
+++ main.py
@@ -35,6 +35,6 @@ async def run_bot():
                 await send_message_safe(message, chart)
                 posted.add(key)
                 await asyncio.sleep(5)
-        await asyncio.sleep(300)  # repeat every 5 min
+        await asyncio.sleep(60)  # check every 1 minute
 
 asyncio.run(run_bot())
