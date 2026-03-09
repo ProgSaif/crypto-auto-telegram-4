c36608af36708661b3a8eeae277c1f265a7db36f
ProgSaif <isaifulone@gmail.com>
2026-03-09 13:02:49 +0600

    Update main.py

--- main.py
+++ main.py
@@ -8,45 +8,32 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL_ID = os.getenv("CHANNEL_ID")
 
 bot = Bot(token=BOT_TOKEN)
-
 posted = set()
 
 
 async def send_message_safe(message):
-
     for i in range(3):
-
         try:
-
             await bot.send_message(
                 chat_id=CHANNEL_ID,
                 text=message
             )
-
             return
-
         except Exception as e:
-
             print("Telegram error:", e)
             await asyncio.sleep(5)
 
 
 async def run_bot():
-
     print("🚀 Signal bot started")
 
     while True:
-
         try:
-
             signals = get_signal_coins()
 
             for s in signals:
-
                 key = f"{s['coin']}_{s['trade_type']}"
-
                 if key not in posted:
-
                     message = generate_signal_message(
                         s["coin"],
                         s["entry"],
@@ -57,17 +44,12 @@ async def run_bot():
                         s["trade_type"],
                         s["confidence"]
                     )
-
                     print("Posting:", s["coin"], s["trade_type"])
-
                     await send_message_safe(message)
-
                     posted.add(key)
-
                     await asyncio.sleep(2)
 
         except Exception as e:
-
             print("Bot error:", e)
 
         await asyncio.sleep(30)
