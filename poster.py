821717beeb4a3fce376240da568241b1a48f3f76
ProgSaif <isaifulone@gmail.com>
2026-03-09 08:16:54 +0600

    Create poster.py

---
+++ poster.py
@@ -0,0 +1,11 @@
+def generate_post(coin):
+
+    return f"""
+Guys! ${coin} created another bullish movement
+
+People lie but chart don’t
+
+#{coin} going to more down in near future
+
+— Follow for more real updates —
+"""
