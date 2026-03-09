d4152163dfbef0cccdb6795353279e993bfca2c6
ProgSaif <isaifulone@gmail.com>
2026-03-09 11:14:49 +0600

    Update main.py

--- main.py
+++ main.py
@@ -10,14 +10,10 @@ CHANNEL_ID = os.getenv("CHANNEL_ID")
 bot = Bot(token=BOT_TOKEN)
 posted = set()
 
-async def send_message_safe(message, chart=None):
+async def send_message_safe(message):
     for i in range(3):
         try:
-            if chart:
-                with open(chart, "rb") as img:
-                    await bot.send_photo(chat_id=CHANNEL_ID, photo=img, caption=message)
-            else:
-                await bot.send_message(chat_id=CHANNEL_ID, text=message)
+            await bot.send_message(chat_id=CHANNEL_ID, text=message)
             return
         except Exception as e:
             print("Telegram error:", e)
@@ -30,11 +26,10 @@ async def run_bot():
             key = f"{s['coin']}_{s['trade_type']}"
             if key not in posted:
                 message = generate_signal_message(s)
-                chart = s.get("chart_file")
                 print("Posting:", s['coin'], s['trade_type'])
-                await send_message_safe(message, chart)
+                await send_message_safe(message)
                 posted.add(key)
-                await asyncio.sleep(5)
-        await asyncio.sleep(60)  # check every 1 minute
+                await asyncio.sleep(2)
+        await asyncio.sleep(10)  # fast testing loop
 
 asyncio.run(run_bot())
