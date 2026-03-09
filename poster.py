50d81d18317cb4f2f1fdebf01031a2fc6b1e413d
ProgSaif <isaifulone@gmail.com>
2026-03-09 08:56:12 +0600

    Update poster.py

--- poster.py
+++ poster.py
@@ -2,11 +2,11 @@ def generate_signal_message(coin, entry, sl, tp1, tp2, tp3, trade_type="LONG", c
     return f"""
 ${coin} – {trade_type}
 
-Entry: {entry:.2f}
-SL: {sl:.2f}
-TP1: {tp1:.2f}
-TP2: {tp2:.2f}
-TP3: {tp3:.2f}
+Entry: {entry:.5f}
+SL: {sl:.5f}
+TP1: {tp1:.5f}
+TP2: {tp2:.4f}
+TP3: {tp3:.4f}
 
 Why this setup?
 • Confidence: {confidence}%
